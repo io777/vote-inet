20b5f0098a677fd8e36b31914ddf1542d7c7e89d
Daddy Cool <shirling@mail.ru>
2015-10-11 19:20:05 +0500

    edite pretty email

--- server/server_verify_email.js
+++ server/server_verify_email.js
@@ -1,30 +1,43 @@
 Meteor.startup(function () {
-  // test with gmail
-  
-  process.env.MAIL_URL='smtp://opros.intreneta%40mail.ru:' + encodeURIComponent("{thdfvdctv777") + '@smtp.mail.ru:465';
-  // test with SendGrid
-  // process.env.MAIL_URL = 'smtp://your_username:your_password@smtp.sendgrid.net:587';
-  console.log(process.env);
+	// test with gmail
+	
+	process.env.MAIL_URL='smtp://opros.intreneta%40mail.ru:' + encodeURIComponent("{thdfvdctv777") + '@smtp.mail.ru:465';
+	// test with SendGrid
+	// process.env.MAIL_URL = 'smtp://your_username:your_password@smtp.sendgrid.net:587';
+	console.log(process.env);
 });
 
 // Accounts.emailTemplates.siteName = "Опрос интернета";
 // Accounts.emailTemplates.from = "Опрос интренета <opros.intreneta@mail.ru>";
 
 // if (Meteor.isServer) {
-    // Email.send({
-    //     from: 'server@example.com',
-    //     to: 'shirling@mail.ru',
-    //     subject: 'This is a test email',
-    //     html: '<b>Congrats, it works!</b>'
-    // });
-  
-// PrettyEmail.send('call-to-action', {
-//   to: 'maddesire@inbox.ru',
-//   subject: 'You got new message',
-//   heading: 'Your friend sent you a message',
-//   message: 'Click the button below to read the message',
-//   buttonText: 'Read message',
-//   buttonUrl: 'http://mycompany.com/messages/2314'
-//   });
-
+		// Email.send({
+		//     from: 'server@example.com',
+		//     to: 'shirling@mail.ru',
+		//     subject: 'This is a test email',
+		//     html: '<b>Congrats, it works!</b>'
+		// });
 // }
+
+PrettyEmail.options = {
+	from: 'opros.intreneta@mail.ru',
+	logoUrl: '/images/computer-database-psd-icon-mini.png',
+	companyName: 'Maddesire',
+	companyUrl: 'http://maddesire.net',
+	companyAddress: 'maddesire.net',
+	companyTelephone: 'mad.desire.db',
+	companyEmail: 'maddesire@inbox.ru',
+	siteName: 'Опрос интернета'
+};
+
+PrettyEmail.defaults.verifyEmail = {
+	heading: 'Всего один шаг...',
+	message: 'Нажмите на большую кнопку ниже, чтобы активировать свой аккаунт',
+	buttonText: 'Активировать аккаунт'
+};
+
+PrettyEmail.defaults.resetPassword = {
+	heading: 'Сброс пароля',
+	message: 'Нажмите большую кнопку ниже, чтобы сбросить пароль',
+	buttonText: 'Сброс пароля'
+};
